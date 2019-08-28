fd6559c32f6b35d2837dc429beeb9e6e53553d1e
513004719 <513004719@AYCAPLW0DJ5DNF2.AYCAP.BAYAD.CO.TH>
2019-08-28 16:31:40 +0700

    first commit

--- demo/src/main/java/com/utilities/OracleJdbcTest.java
+++ demo/src/main/java/com/utilities/OracleJdbcTest.java
@@ -12,7 +12,7 @@ public static void main(String args[])
 	try{  
 //step1 load the driver class  
 	Class.forName("oracle.jdbc.driver.OracleDriver");  
-	  
+	  //test
 	//step2 create  the connection object  
 	//Connection con=DriverManager.getConnection("jdbc:oracle:thin:@DESKTOP-02RCN7L:1522/XE","system","55555"); 
 	Connection con = DriverManager.getConnection("jdbc:oracle:thin:@dbcondev2.aycap.bayad.co.th:1588/aycapd"," ibb_app_uat5"," ibb_app_uat5_11g");
